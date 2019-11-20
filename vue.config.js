c11c0ae3c476b3b2253e5ffc270457a693282669
lianming <dushi@dushideMacBook-Air.local>
2019-11-20 18:24:09 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
   runtimeCompiler: true,
-  publicPath: 'muscicPlayer',
+  publicPath: 'muscicPlayer/v0001',
 
   chainWebpack(config) {
     config.resolve.alias
