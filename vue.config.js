a039698f8ae63f9da387f3d82dc41041cef5f0df
lianming <dushi@dushideMacBook-Air.local>
2019-12-11 17:32:22 +0800

    增加版本号

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,8 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
+const version = "1.0.0"
+
 module.exports = {
   runtimeCompiler: true,
   publicPath: 'muscicPlayer',
@@ -11,5 +13,11 @@ module.exports = {
   chainWebpack(config) {
     config.resolve.alias
       .set('@', resolve('src/'))
+  },
+  configureWebpack: { // webpack 配置
+    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+      filename: `[name].${version}.${Timestamp}.js`,
+      chunkFilename: `[name].${version}.${Timestamp}.js`
+    }
   }
 }
